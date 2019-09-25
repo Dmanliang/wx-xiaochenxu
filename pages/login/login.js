a2500e3822e7a5016d9bcc03b2b46463331e28f7
Decml <454580993@qq.com>
2019-09-25 17:21:37 +0800

    登录注册,下拉列表
    登录注册页面完善,表单form的使用,button按钮的监听,scroll-view的横向,纵向分布滑动

--- pages/login/login.js
+++ pages/login/login.js
@@ -5,62 +5,79 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    text_login: "登录",
+    text_register: "注册",
+    loading_login: false,
+    loading_register: false,
   },
+
+  // 当用户提交表单回调这个函数
+  onbindsubmit: function(event) {
+    console.log("username---->" + event.detail.value.username),
+    console.log("passwrod---->" + event.detail.value.password)
+  },
+
+  clickRegister: function(event) {
+    this.setData({
+      loading_register: !this.data.loading_register,
+      text_register: "Register"
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
